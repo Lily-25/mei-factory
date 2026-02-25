dc539d4b061cd81ddb051786111fbce4de368898
meimei <m02062303@163.com>
2026-02-25 19:17:53 +0800

    assignment

--- src/IndividualAssignment/utiles/llm_api.py
+++ src/IndividualAssignment/utiles/llm_api.py
@@ -1,14 +1,14 @@
 
 from llama_index.llms.deepseek import DeepSeek
 
-deepseek_key = "sk-0021798633cc402880f809e83b285ede"
+deepseek_key = "sk-"
 
 def get_deepseek_obj():
     llm = DeepSeek(model="deepseek-chat", api_key=deepseek_key)
     return llm
 
 import google.generativeai as genai
-genai.configure(api_key="AIzaSyAORjARWWKJgHYqo3CTIVPeIu2Yg5qI550")
+genai.configure(api_key="")
 
 def get_gemini_obj():
     llm = genai.GenerativeModel("gemini-2.5-flash")
